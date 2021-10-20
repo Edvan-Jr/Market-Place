f893f4b7ca5306cbcefeb5f7d7fa0dfbd71d75cd
Edvan-Jr <edvan.hd@gmail.com>
2021-10-19 21:27:40 -0300

    Configuração da conexão com banco de dados da heroku

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,14 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'postgres',
-        database: 'market_cubos'
+        host: 'ec2-3-225-204-194.compute-1.amazonaws.com',
+        user: 'ncfyqqpedmafac',
+        password: '0e17be582895c64cc806e8c6905faa2a6599635da885467d5a4bc228488bdd3e',
+        port: 5432,
+        database: 'd1tc21ibnjgeq8',
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
